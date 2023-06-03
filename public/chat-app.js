42909e974d3fa475561b8efae968eb1519fa7f2a
Tishadubey01 <tisha.dubey@students.iiit.ac.in>
2023-06-04 03:17:59 +0530

    functionality done

--- public/chat-app.js
+++ public/chat-app.js
@@ -1,4 +1,4 @@
-// Firebase Authentication and Firestore instances
+// // // Firebase Authentication and Firestore instances
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
@@ -67,13 +67,35 @@ loginButton.addEventListener("click", () => {
   auth.signInWithPopup(provider)
     .then((result) => {
       console.log("Login successful");
+      console.log("Display Name:", auth.currentUser.displayName);
+console.log("Email Address:", auth.currentUser.email);
+
       toggleContainers(false);
       userName.textContent = result.user.displayName;
+     
+      // Save user information to Firestore
+      const userRef = firestore.collection("users").doc(result.user.uid);
+
+      userRef.set({
+        userId: result.user.uid,
+        username: result.user.displayName,
+        email: result.user.email,
+        profilePicture: result.user.photoURL
+      })
+      .then(() => {
+        console.log("User information saved successfully");
+      })
+      
+      .catch((error) => {
+        console.error("Error saving user information:", error);
+      });
+     
     })
     .catch((error) => {
       console.error("Login error:", error);
     });
 });
+
 
 
 // Create room button click event
@@ -85,6 +107,7 @@ createRoomButton.addEventListener("click", () => {
         .add({ name: roomName })
         .then(() => {
           console.log("Room created successfully");
+          window.prompt("Room created successfully");
         })
         .catch((error) => {
           console.error("Error creating room:", error);
@@ -128,3 +151,252 @@ auth.onAuthStateChanged((user) => {
     userName.textContent = "";
   }
 });
+const directMessageButton = document.getElementById("direct-message-button");
+directMessageButton.addEventListener("click", () => {
+  // Fetch the list of available users (excluding the logged-in user)
+  firestore.collection("users")
+    .where("userId", "!=", auth.currentUser.uid)
+    .get()
+    .then((querySnapshot) => {
+      const users = [];
+      querySnapshot.forEach((doc) => {
+        users.push({
+          id: doc.id,
+          username: doc.data().username
+        });
+      });
+      displayUsers(users);
+    })
+    .catch((error) => {
+      console.error("Error fetching users:", error);
+    });
+});
+
+function displayUsers(users) {
+  // Clear existing user list
+  const usersList = document.getElementById("users-list");
+  usersList.innerHTML = "";
+
+  users.forEach((user) => {
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("user");
+
+    const username = document.createElement("span");
+    username.textContent = user.username;
+    userDiv.appendChild(username);
+
+    const chatButton = document.createElement("button");
+    chatButton.textContent = "Chat";
+    chatButton.addEventListener("click", () => {
+      // Navigate to the direct messaging page with the selected user
+      window.location.href = "chat.html?userId=" + user.id;
+    });
+    userDiv.appendChild(chatButton);
+
+    usersList.appendChild(userDiv);
+  });
+}
+
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
+
+// // DOM elements
+// const loginContainer = document.getElementById("login-container");
+// const chatContainer = document.getElementById("chat-container");
+// const loginButton = document.getElementById("login-button");
+// const createRoomButton = document.getElementById("create-room-button");
+// const viewRoomsButton = document.getElementById("view-rooms-button");
+// const directMessageButton = document.getElementById("direct-message-button");
+// const roomsList = document.getElementById("rooms-list");
+// const usersList = document.getElementById("users-list");
+// const userName = document.getElementById("user-name");
+
+// // Function to toggle visibility of login and chat containers
+// function toggleContainers(showLogin) {
+//   if (showLogin) {
+//     loginContainer.style.display = "block";
+//     chatContainer.style.display = "none";
+//   } else {
+//     loginContainer.style.display = "none";
+//     chatContainer.style.display = "block";
+//   }
+// }
+
+// // Signout button click event
+// const signoutButton = document.getElementById("signout-button");
+// signoutButton.addEventListener("click", () => {
+//   auth.signOut()
+//     .then(() => {
+//       console.log("User signed out");
+//       toggleContainers(true);
+//     })
+//     .catch((error) => {
+//       console.error("Signout error:", error);
+//     });
+// });
+
+// // Function to display rooms
+// function displayRooms(rooms) {
+//   roomsList.innerHTML = ""; // Clear existing list
+
+//   rooms.forEach((room) => {
+//     const roomDiv = document.createElement("div");
+//     roomDiv.classList.add("room");
+
+//     const roomName = document.createElement("span");
+//     roomName.textContent = room.name;
+//     roomDiv.appendChild(roomName);
+
+//     const joinButton = document.createElement("button");
+//     joinButton.textContent = "Join Room";
+//     joinButton.addEventListener("click", () => {
+//       // Navigate to the chat page for the selected room
+//       window.location.href = "chat.html?roomId=" + room.id;
+//     });
+//     roomDiv.appendChild(joinButton);
+
+//     roomsList.appendChild(roomDiv);
+//   });
+// }
+// function displayUsers(users) {
+//   usersList.innerHTML = ""; // Clear existing users
+
+//   users.forEach((user) => {
+//     const userDiv = document.createElement("div");
+//     userDiv.classList.add("user");
+
+//     const usernameSpan = document.createElement("span");
+//     usernameSpan.textContent = user.username;
+//     userDiv.appendChild(usernameSpan);
+
+//     const chatButton = document.createElement("button");
+//     chatButton.textContent = "Chat";
+//     chatButton.addEventListener("click", () => {
+//       // Navigate to the direct messaging page for the selected user
+//       window.location.href = "direct.html?userId=" + user.userId;
+//     });
+//     userDiv.appendChild(chatButton);
+
+//     usersList.appendChild(userDiv);
+//   });
+// }
+// // Function to display users
+// // function displayUsers(users) {
+// //   usersList.innerHTML = ""; // Clear existing list
+
+// //   users.forEach((user) => {
+// //     const userDiv = document.createElement("div");
+// //     userDiv.classList.add("user");
+
+// //     const userName = document.createElement("span");
+// //     userName.textContent = user.username;
+// //     userDiv.appendChild(userName);
+
+// //     const chatButton = document.createElement("button");
+// //     chatButton.textContent = "Chat";
+// //     chatButton.addEventListener("click", () => {
+// //       // Navigate to the chat page for the selected user
+// //       window.location.href = "direct.html?userId=" + user.userId;
+// //     });
+// //     userDiv.appendChild(chatButton);
+
+// //     //usersList.appendChild(userDiv);
+// //   });
+// // }
+
+// // Login with Google button click event
+// loginButton.addEventListener("click", () => {
+//   const provider = new firebase.auth.GoogleAuthProvider();
+
+//   auth.signInWithPopup(provider)
+//     .then((result) => {
+//       console.log("Login successful");
+//       toggleContainers(false);
+//       userName.textContent = result.user.displayName;
+
+//       // Save user information to Firestore
+//       const userRef = firestore.collection("users").doc(result.user.uid);
+
+//       userRef.set({
+//         userId: result.user.uid,
+//         username: result.user.displayName,
+//         email: result.user.email
+//       })
+//         .then(() => {
+//           console.log("User information saved successfully");
+//         })
+//         .catch((error) => {
+//           console.error("Error saving user information:", error);
+//         });
+//     })
+//     .catch((error) => {
+//       console.error("Login error:", error);
+//     });
+// });
+
+// // Create Room button click event
+// createRoomButton.addEventListener("click", () => {
+//   const roomName = prompt("Enter room name:");
+//   if (roomName) {
+//     const roomRef = firestore.collection("rooms").doc();
+//     roomRef.set({
+//       id: roomRef.id,
+//       name: roomName
+//     })
+//       .then(() => {
+//         console.log("Room created successfully");
+//       })
+//       .catch((error) => {
+//         console.error("Error creating room:", error);
+//       });
+//   }
+// });
+
+// // View Rooms button click event
+// viewRoomsButton.addEventListener("click", () => {
+//   firestore.collection("rooms")
+//     .get()
+//     .then((querySnapshot) => {
+//       const rooms = [];
+//       querySnapshot.forEach((doc) => {
+//         rooms.push(doc.data());
+//       });
+//       displayRooms(rooms);
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching rooms:", error);
+//     });
+// });
+
+// // Direct Message button click event
+// directMessageButton.addEventListener("click", () => {
+//   firestore.collection("users")
+//     .get()
+//     .then((querySnapshot) => {
+//       const users = [];
+//       querySnapshot.forEach((doc) => {
+//         const user = doc.data();
+//         if (user.userId !== auth.currentUser.uid) {
+//           users.push(user);
+//         }
+//       });
+//       displayUsers(users);
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching users:", error);
+//     });
+// });
+
+// // Check if a user is already logged in
+// auth.onAuthStateChanged((user) => {
+//   if (user) {
+//     console.log("User is logged in:", user);
+//     toggleContainers(false);
+//     userName.textContent = user.displayName;
+//   } else {
+//     console.log("No user logged in");
+//     toggleContainers(true);
+//     userName.textContent = "";
+//   }
+// });
+
--- public/chat-room.js
+++ public/chat-room.js
@@ -1,6 +1,208 @@
-// Get the room ID from the query string
+
+
+
+
+
+
+
+
+// // Get the room ID or user ID from the query string
+// const urlParams = new URLSearchParams(window.location.search);
+// const roomId = urlParams.get("roomId");
+// const userId = urlParams.get("userId");
+
+// // Firebase Authentication and Firestore instances
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
+
+// // DOM elements
+// const messagesDiv = document.getElementById("messages");
+// const messageInput = document.getElementById("message-input");
+// const sendButton = document.getElementById("send-button");
+// const leaveButton = document.getElementById("leave-button");
+// const chatHeading = document.getElementById("chat-heading");
+
+// function displayMessage(message) {
+//   const { text, senderName, senderProfilePicture } = message;
+
+//   const messageDiv = document.createElement("div");
+//   messageDiv.classList.add("message");
+
+//   // Create an element for the profile picture
+//   const profilePicture = document.createElement("img");
+//   profilePicture.classList.add("profile-picture");
+//   profilePicture.src = senderProfilePicture;
+//   messageDiv.appendChild(profilePicture);
+
+//   // Create an element for the sender's name
+//   const senderNameElement = document.createElement("span");
+//   senderNameElement.classList.add("sender-name");
+//   senderNameElement.textContent = senderName;
+//   messageDiv.appendChild(senderNameElement);
+
+//   // Create an element for the message text
+//   const messageText = document.createElement("p");
+//   messageText.classList.add("message-text");
+//   messageText.textContent = text;
+//   messageDiv.appendChild(messageText);
+
+//   messagesDiv.appendChild(messageDiv);
+// }
+
+// // Real-time message updates
+// if (roomId) {
+//   firestore
+//     .collection("rooms")
+//     .doc(roomId)
+//     .collection("messages")
+//     .orderBy("timestamp")
+//     .onSnapshot((snapshot) => {
+//       snapshot.docChanges().forEach((change) => {
+//         if (change.type === "added") {
+//           const message = change.doc.data();
+//           displayMessage(message);
+//         }
+//       });
+//     });
+
+//   // Display previous messages
+//   firestore
+//     .collection("rooms")
+//     .doc(roomId)
+//     .collection("messages")
+//     .orderBy("timestamp")
+//     .get()
+//     .then((querySnapshot) => {
+//       querySnapshot.forEach((doc) => {
+//         const message = doc.data();
+//         displayMessage(message);
+//       });
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching previous messages:", error);
+//     });
+
+//   // Send message button click event
+//   sendButton.addEventListener("click", () => {
+//     const message = messageInput.value;
+//     messageInput.value = "";
+
+//     const user = auth.currentUser;
+//     const { displayName, photoURL, uid } = user;
+
+//     firestore
+//       .collection("rooms")
+//       .doc(roomId)
+//       .collection("messages")
+//       .add({
+//         text: message,
+//         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+//         userId: uid,
+//         senderName: displayName,
+//         senderProfilePicture: photoURL,
+//       })
+//       .then(() => {
+//         console.log("Message sent successfully");
+//       })
+//       .catch((error) => {
+//         console.error("Message send error:", error);
+//       });
+//   });
+// } else if (userId) {
+//   // Fetch the user information for direct messaging
+//   firestore
+//     .collection("users")
+//     .doc(userId)
+//     .get()
+//     .then((doc) => {
+//       const user = doc.data();
+//       if (user) {
+//         const { username, profilePicture } = user;
+//         chatHeading.textContent = "Chatting with " + username;
+
+//         firestore
+//           .collection("users")
+//           .doc(userId)
+//           .collection("messages")
+//           .orderBy("timestamp")
+//           .onSnapshot((snapshot) => {
+//             snapshot.docChanges().forEach((change) => {
+//               if (change.type === "added") {
+//                 const message = change.doc.data();
+//                 displayMessage(message);
+//               }
+//             });
+//           });
+
+//         // Display previous messages
+//         firestore
+//           .collection("users")
+//           .doc(userId)
+//           .collection("messages")
+//           .orderBy("timestamp")
+//           .get()
+//           .then((querySnapshot) => {
+//             querySnapshot.forEach((doc) => {
+//               const message = doc.data();
+//               displayMessage(message);
+//             });
+//           })
+//           .catch((error) => {
+//             console.error("Error fetching previous messages:", error);
+//           });
+
+//         // Send message button click event
+//         sendButton.addEventListener("click", () => {
+//           const message = messageInput.value;
+//           messageInput.value = "";
+
+//           const user = auth.currentUser;
+//           const { displayName, photoURL, uid } = user;
+
+//           firestore
+//             .collection("users")
+//             .doc(userId)
+//             .collection("messages")
+//             .add({
+//               text: message,
+//               timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+//               senderId: uid,
+//               senderName: displayName,
+//               senderProfilePicture: photoURL,
+//             })
+//             .then(() => {
+//               console.log("Message sent successfully");
+//             })
+//             .catch((error) => {
+//               console.error("Message send error:", error);
+//             });
+//         });
+//       } else {
+//         console.error("User information not found");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching user information:", error);
+//     });
+// }
+
+// function getDirectMessageId(userId1, userId2) {
+//   const ids = [userId1, userId2];
+//   ids.sort();
+//   return ids.join("-");
+// }
+
+// // Leave button click event
+// leaveButton.addEventListener("click", () => {
+//   window.history.back();
+// });
+
+
+
+// Get the room ID or user ID from the query string
 const urlParams = new URLSearchParams(window.location.search);
 const roomId = urlParams.get("roomId");
+const userId = urlParams.get("userId");
 
 // Firebase Authentication and Firestore instances
 const auth = firebase.auth();
@@ -11,37 +213,41 @@ const messagesDiv = document.getElementById("messages");
 const messageInput = document.getElementById("message-input");
 const sendButton = document.getElementById("send-button");
 const leaveButton = document.getElementById("leave-button");
+const chatHeading = document.getElementById("chat-heading");
 
 function displayMessage(message) {
-    const { text, senderName, senderProfilePicture } = message;
-  
-    const messageDiv = document.createElement("div");
-    messageDiv.classList.add("message");
-  
-    // Create an element for the profile picture
-    const profilePicture = document.createElement("img");
-    profilePicture.classList.add("profile-picture");
-    profilePicture.src = senderProfilePicture;
-    messageDiv.appendChild(profilePicture);
-  
-    // Create an element for the sender's name
-    const senderNameElement = document.createElement("span");
-    senderNameElement.classList.add("sender-name");
-    senderNameElement.textContent = senderName;
-    messageDiv.appendChild(senderNameElement);
-  
-    // Create an element for the message text
-    const messageText = document.createElement("p");
-    messageText.classList.add("message-text");
-    messageText.textContent = text;
-    messageDiv.appendChild(messageText);
-  
-    messagesDiv.appendChild(messageDiv);
-  }
-  
-  
-  // Real-time message updates
-  firestore.collection("rooms").doc(roomId).collection("messages")
+  const { text, senderName, senderProfilePicture } = message;
+
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+
+  // Create an element for the profile picture
+  const profilePicture = document.createElement("img");
+  profilePicture.classList.add("profile-picture");
+  profilePicture.src = senderProfilePicture;
+  messageDiv.appendChild(profilePicture);
+
+  // Create an element for the sender's name
+  const senderNameElement = document.createElement("span");
+  senderNameElement.classList.add("sender-name");
+  senderNameElement.textContent = senderName;
+  messageDiv.appendChild(senderNameElement);
+
+  // Create an element for the message text
+  const messageText = document.createElement("p");
+  messageText.classList.add("message-text");
+  messageText.textContent = text;
+  messageDiv.appendChild(messageText);
+
+  messagesDiv.appendChild(messageDiv);
+}
+
+// Real-time message updates
+if (roomId) {
+  firestore
+    .collection("rooms")
+    .doc(roomId)
+    .collection("messages")
     .orderBy("timestamp")
     .onSnapshot((snapshot) => {
       snapshot.docChanges().forEach((change) => {
@@ -51,9 +257,12 @@ function displayMessage(message) {
         }
       });
     });
-  
+
   // Display previous messages
-  firestore.collection("rooms").doc(roomId).collection("messages")
+  firestore
+    .collection("rooms")
+    .doc(roomId)
+    .collection("messages")
     .orderBy("timestamp")
     .get()
     .then((querySnapshot) => {
@@ -65,20 +274,15 @@ function displayMessage(message) {
     .catch((error) => {
       console.error("Error fetching previous messages:", error);
     });
-  
-  
-  
-  
-  
 
-// Send message button click event
-sendButton.addEventListener("click", () => {
+  // Send message button click event
+  sendButton.addEventListener("click", () => {
     const message = messageInput.value;
     messageInput.value = "";
-  
+
     const user = auth.currentUser;
     const { displayName, photoURL, uid } = user;
-  
+
     firestore
       .collection("rooms")
       .doc(roomId)
@@ -97,41 +301,97 @@ sendButton.addEventListener("click", () => {
         console.error("Message send error:", error);
       });
   });
-  
+} else if (userId) {
+  // Fetch the user information for direct messaging
+  firestore
+    .collection("users")
+    .doc(userId)
+    .get()
+    .then((doc) => {
+      const user = doc.data();
+      if (user) {
+        const { username, profilePicture } = user;
+        chatHeading.textContent = "Chatting with " + username;
 
+        const currentUser = auth.currentUser;
+        const { uid } = currentUser;
 
-// Leave room button click event
+        const directMessageId = getDirectMessageId(uid, userId);
+
+        firestore
+          .collection("direct_messages")
+          .doc(directMessageId)
+          .collection("messages")
+          .orderBy("timestamp")
+          .onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+              if (change.type === "added") {
+                const message = change.doc.data();
+                displayMessage(message);
+              }
+            });
+          });
+
+        // Display previous messages
+        firestore
+          .collection("direct_messages")
+          .doc(directMessageId)
+          .collection("messages")
+          .orderBy("timestamp")
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              const message = doc.data();
+              displayMessage(message);
+            });
+          })
+          .catch((error) => {
+            console.error("Error fetching previous messages:", error);
+          });
+
+        // Send message button click event
+        sendButton.addEventListener("click", () => {
+          const message = messageInput.value;
+          messageInput.value = "";
+
+          const user = auth.currentUser;
+          const { displayName, photoURL, uid } = user;
+
+          firestore
+            .collection("direct_messages")
+            .doc(directMessageId)
+            .collection("messages")
+            .add({
+              text: message,
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              senderId: uid,
+              senderName: displayName,
+              senderProfilePicture: photoURL,
+            })
+            .then(() => {
+              console.log("Message sent successfully");
+            })
+            .catch((error) => {
+              console.error("Message send error:", error);
+            });
+        });
+      } else {
+        console.error("User information not found");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching user information:", error);
+    });
+}
+
+function getDirectMessageId(userId1, userId2) {
+  const ids = [userId1, userId2];
+  ids.sort();
+  return ids.join("-");
+}
+
+// Leave button click event
 leaveButton.addEventListener("click", () => {
-    
-    // Add your code here to leave the chat room
-    auth.signOut()
-      .then(() => {
-        console.log("User logged out");
-        window.location.href = "index.html"; // Navigate to homepage.html
-      })
-      .catch((error) => {
-        console.error("Logout error:", error);
-      });
-  });
-  
-  
-
-// Real-time message updates
-// firestore.collection("rooms").doc(roomId).collection("messages")
-//   .orderBy("timestamp")
-//   .onSnapshot((snapshot) => {
-//     snapshot.docChanges().forEach((change) => {
-//       if (change.type === "added") {
-//         const message = change.doc.data();
-//         displayMessage(message.text);
-//       }
-//     });
-//   });
-
-// Check if a user is already logged in
-auth.onAuthStateChanged((user) => {
-  if (!user) {
-    console.log("No user logged in");
-    window.location.href = "index.html";
-  }
+  window.history.back();
 });
+
